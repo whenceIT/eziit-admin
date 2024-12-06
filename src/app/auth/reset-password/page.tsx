b3d8473207cd14977ecaf2747204fe92dfe34f86
whenceIT <whenceit@gmail.com>
2024-12-06 13:50:18 +0200

    reset password removed

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,17 +1,2 @@
-/*'use client';
-
-import * as React from 'react';
-import { useParams } from 'next/navigation';
-import { Layout } from '@/components/auth/layout';
-import { ResetPasswordForm } from '@/components/auth/reset-password-form';
-
-export default function ResetPasswordPage(): React.JSX.Element {
-  const params = useParams();
-  const token = params.token as string;
-
-  return (
-    <Layout>
-      <ResetPasswordForm token={token} />
-    </Layout>
-  );
-}*/
+/*
+*/
